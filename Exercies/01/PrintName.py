0ba33e89d7e3bd4774b1847deb24a7aba07f4fe9
tocee123 <93769005+tocee123@users.noreply.github.com>
2023-11-14 23:04:23 +0100

    update git ignore

--- Exercies/01/PrintName.py
+++ Exercies/01/PrintName.py
@@ -1,8 +1,1 @@
-separator = " "
-sentence = ""
-while True:
-    word = input("Give me a word or press '.' to exit: ")
-    if word == ".":
-        break
-    sentence += f"{word}{separator}"
-print(f"The given sentence is: {sentence}\nThe sentence contains {sentence.count(separator)} words")
+print("Name")
