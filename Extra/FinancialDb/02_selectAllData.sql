1b6b88619df7e392c91b58695e4ae43a6ec9625e
TamasT <93769005+tocee123@users.noreply.github.com>
2025-01-19 09:41:13 +0100

    New sql files

---
+++ Extra/FinancialDb/02_selectAllData.sql
@@ -0,0 +1,13 @@
+SELECT 
+    ft.id,
+    ft.amount,
+    ft.date,
+    ft.note,
+    pm.name AS payment_method,
+    tt.name AS transaction_type,
+    c.name AS category
+FROM 
+    financial_transaction ft
+	JOIN payment_method pm ON ft.payment_method_fk = pm.id
+	JOIN transaction_type tt ON ft.transaction_type_fk = tt.id
+	JOIN category c ON ft.category_fk = c.id;
